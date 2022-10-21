5c655407b2639456ea10c1779710ef170985bb53
Felipe Pereira <104787971+Felipesv1@users.noreply.github.com>
2022-10-21 14:07:02 -0300

    Atualizado

--- main.js
+++ main.js
@@ -1,12 +1,13 @@
 const [texto,senha] = document.querySelectorAll('input');
 const btn = document.querySelector('button');
 const img = document.querySelector(".btn");
-
+const ImgLol = document.querySelector('#imgLol')
 function login(){
     if(texto.value && senha.value.length >= 8){
         btn.removeAttribute("disabled")
         img.classList.remove("btn")
        img.classList.add("cor-mudar")
+       ImgLol.setAttribute('src','img/league-of-legends.png')
     }else{
         btn.setAttribute("disabled","disabled");
         img.classList.add("btn")
